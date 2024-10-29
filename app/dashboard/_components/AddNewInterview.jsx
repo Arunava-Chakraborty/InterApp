44616fb8705693ce813427e65015c5bd5eb1227e
Arunava Chakraborty <118994880+Arunava-Chakraborty@users.noreply.github.com>
2024-10-29 21:26:16 +0530

    sdcbaocdhn

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -12,15 +12,14 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
 import { chatSession } from '@/utils/GeminiAi'
-import { json } from 'drizzle-orm/mysql-core'
+import { json } from 'drizzle-orm/pg-core'
 import { LoaderCircle } from 'lucide-react'
 import { db } from '@/utils/db'
 import { MockInterview } from '@/utils/schema'
-import { v4 as uuidv4 } from 'uuid';
 import { useUser } from '@clerk/nextjs'
 import moment from 'moment/moment'
 import { useRouter } from 'next/navigation'
-
+import { v4 as uuidv4 } from 'uuid';
 
 function AddNewInterview() {
   const [openDialog , setOpenDialog]= useState(false);
@@ -54,12 +53,12 @@ function AddNewInterview() {
             jobExperience: jobExperience,
             createdBy : user?.primaryEmailAddress?.emailAddress,
             createdAt : moment().format('YYYY-MM-DD HH:mm:ss')
-        }).returning({mockID:MockInterview.mockID})
+        }).returning({mockID:MockInterview.mockID});
 
         console.log("Inserted ID" , resp) 
         if(resp){
           setOpenDialog(false);
-          router.push('/dashboard/interview/' + resp[0]?.mockID)
+          router.push('/dashboard/interview/'+resp[0]?.mockID)
 
         }
       }
--- app/dashboard/interview/[interviewId]/page.jsx
+++ app/dashboard/interview/[interviewId]/page.jsx
@@ -1,24 +1,56 @@
-'use client'
+"use client"
+import { Button } from '@/components/ui/button'
 import { db } from '@/utils/db'
 import { MockInterview } from '@/utils/schema'
 import { eq } from 'drizzle-orm'
-import React, { useEffect } from 'react'
+import { WebcamIcon } from 'lucide-react'
+import React, { useEffect, useState } from 'react'
+import Webcam from 'react-webcam'
 
 function Interview(params) {
+    const [interviewData , setInterviewData] = useState();
+    const [webCamEnable , setWebCamEnable] = useState(false);
+
     useEffect(()=>{
         console.log(params.interviewId)
         GetInterviewDetails();
-    }, [])
+    }, []);
 
-    const GetInterviewDetails = async()=>{
-        const result = await db.select().from(MockInterview).where(eq(MockInterview.mockID,params.interviewId))
+    const GetInterviewDetails = async() =>{
+        try{
+        const result = await db.select().from(MockInterview).where(eq(MockInterview.mockID, params.interviewId))
+        console.log(result);
+        setInterviewData(result);
+        } catch(err){
+            console.log(err)
+        }
         
-        console.log(result)
-
     }
   return (
-    <div>
-        Interview</div>
+    <div className='my-10 flex justify-center flex-col items-center'>
+        <h2 className=' font-bold text-2xl'>Lets start </h2>
+        <div>
+            {webCamEnable?<Webcam
+            onUserMedia={()=>setWebCamEnable(true)}
+            onUserMediaError={()=>setWebCamEnable(false)}
+            mirrored = {true}
+            style={{
+                height:300,
+                width:300
+            }}
+            />
+            :<>
+            <WebcamIcon className='h-72 w-full p-10 bg-secondary rounded-lg border my-4' 
+            />
+            <Button onClick={()=>setWebCamEnable(true)}>Start Camera</Button>
+            </>
+        }
+        </div>
+        <div>
+            <h2><strong>JOB ROLE/DESCRIPTION</strong>{/*interviewData.jobPosition*/} </h2>
+        </div>
+
+    </div>
   )
 }
 
